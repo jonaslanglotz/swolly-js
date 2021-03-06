71286e8feb64c6bbec86b3ff96319e5fb5d2f1ff
Jonas Langlotz <jonas.l1512@gmail.com>
2021-03-06 11:54:28 +0100

    added models to export

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 const Errors = require("./errors")
 const Enums = require("./enums")
 
+const Model = require("./models/model")
+const Category = require("./models/category")
+const Image = require("./models/image")
+const Project = require("./models/project")
+const Session = require("./models/session")
+const Task = require("./models/task")
+const User = require("./models/user")
+
 module.exports = {
     Swolly: require("./swolly"),
     Errors,
     Enums,
+
+    Model,
+    Category,
+    Image,
+    Project,
+    Session,
+    Task,
+    User,
 
     ...Errors
 }
